caeed8ca4d5bb6301892f4ce437b8195f7a8ee6a
sudharma-network <vkw143download@gmail.com>
2026-08-29 10:24:35 +0530

    feat(gpu-pow): define immutable chain pow policy

--- pow/gpu_pow_v1_activation.go
+++ pow/gpu_pow_v1_activation.go
@@ -1,17 +1,13 @@
 package pow
 
-import "github.com/sudharma-networks/sudharma/params"
+import "github.com/sudharma-networks/sudharma/blockchain"
 
 // GPUV1VersionAllowedAtHeight applies the Version-1/Version-2 activation
 // boundary without selecting a network or arming an activation height.
 // Before activation only legacy Version 1 is valid; from activation onward
 // only GPU-PoW Version 2 is valid. Future versions are rejected explicitly.
 func GPUV1VersionAllowedAtHeight(version uint32, height, activationHeight uint64) bool {
-	if activationHeight == params.GPUV1ActivationDisabled {
-		return version == 1
-	}
-	if height < activationHeight {
-		return version == 1
-	}
-	return version == 2
+	return blockchain.PoWPolicy{
+		GPUV1ActivationHeight: activationHeight,
+	}.VersionAllowed(version, height)
 }
