8b39c160d406b7aa4aa9c559a3d29b5c16ef25c4
sudharma-network <vkw143download@gmail.com>
2026-08-31 15:58:55 +0530

    feat(consensus): define network monetary policies

---
+++ params/monetary.go
@@ -0,0 +1,36 @@
+package params
+
+import "fmt"
+
+type MonetaryPolicy uint8
+
+const (
+	MonetaryPolicyPublicTestnet MonetaryPolicy = 1
+	MonetaryPolicyMainnet       MonetaryPolicy = 2
+
+	MainnetMaxSupplySUDH       uint64 = 51_000_000
+	MainnetMaxSupply                  = MainnetMaxSupplySUDH * CoinDecimals
+	MainnetFinalSubsidyHeight uint64 = 5_259_600
+	MainnetEpochLength        uint64 = 131_490
+	MainnetEpochCount         uint64 = 40
+)
+
+func ValidateMonetaryPolicy(policy MonetaryPolicy) error {
+	switch policy {
+	case MonetaryPolicyPublicTestnet, MonetaryPolicyMainnet:
+		return nil
+	default:
+		return fmt.Errorf("unknown monetary policy %d", policy)
+	}
+}
+
+func MaxSupplyFor(policy MonetaryPolicy) uint64 {
+	switch policy {
+	case MonetaryPolicyMainnet:
+		return MainnetMaxSupply
+	case MonetaryPolicyPublicTestnet:
+		return MaxSupply
+	default:
+		return 0
+	}
+}
